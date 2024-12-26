0a4ccaa2930b49c0b9bbd13b0c1e7d1339ca47df
its-soa <olumideshode@gmail.com>
2024-12-26 23:56:34 +0000

    update on client resources

--- app/Filament/Resources/ClientResource.php
+++ app/Filament/Resources/ClientResource.php
@@ -11,10 +11,15 @@ use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Infolists\Components\Tabs;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Actions\Action;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\IconColumn;
@@ -30,6 +35,63 @@ class ClientResource extends Resource
     protected static ?string $model = Client::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist->schema([
+            Tabs::make('Label')
+                ->tabs([
+                    Tabs\Tab::make('Personal info')
+                        ->schema([
+                            TextEntry::make('first_name')
+                                ->label('Name')
+                                ->formatStateUsing(fn(string $state, $record): string => $record->first_name . " " . $record->last_name),
+
+                            TextEntry::make('phone'),
+                            TextEntry::make('mobile'),
+
+                            TextEntry::make('email')
+                                ->copyable(),
+
+                            ImageEntry::make('photo'),
+                            TextEntry::make('linkedin')
+                                ->suffixAction(
+                                    Action::make('openLinkedin')
+                                        ->icon('heroicon-m-clipboard')
+                                        ->url(fn($record) => $record->linkedin)
+                                ),
+
+                            TextEntry::make('active')
+                                ->badge()
+                                ->color(fn(bool $state): string => match ($state) {
+                                    false => 'gray',
+                                    true => 'success'
+                                })
+                        ]),
+                    Tabs\Tab::make('Business info')
+                        ->schema([
+                            TextEntry::make('company'),
+                            TextEntry::make('title'),
+                            TextEntry::make('role'),
+                            TextEntry::make('company_website'),
+                            TextEntry::make('business_details'),
+                            TextEntry::make('business_type'),
+                            TextEntry::make('company_size'),
+                            TextEntry::make('company_size'),
+                            TextEntry::make('temperature'),
+
+                        ]),
+                    Tabs\Tab::make('Notes')
+                        ->schema([
+                            TextEntry::make('notes'),
+                            TextEntry::make('referrals'),
+
+                        ]),
+                ])->columnSpanFull()
+
+
+        ]);
+    }
 
     public static function form(Form $form): Form
     {
@@ -95,8 +157,9 @@ class ClientResource extends Resource
                                     TextInput::make('linkedin'),
                                     RichEditor::make('business_details'),
 
-                                    FileUpload::make('photo')->image(),
-                                    // ->maxSize(1024),
+                                    FileUpload::make('photo')
+                                        ->image()
+                                        ->disk('public'),
                                     Select::make('company_size')
                                         ->options([
                                             'small' => 'Small',
@@ -111,7 +174,7 @@ class ClientResource extends Resource
                                         ]),
 
                                 ]),
-                            ]),
+                        ]),
                         Section::make('Other Info')
                             ->description("Client's other information")
                             ->collapsible()
@@ -120,7 +183,7 @@ class ClientResource extends Resource
                                 TextInput::make('referrals'),
                                 TextInput::make('notes'),
                             ]),
-                        ]),
+                    ]),
             ]);
     }
 
@@ -149,7 +212,8 @@ class ClientResource extends Resource
                 //     ->searchable(),
                 TextColumn::make('company_website')
                     ->toggleable()
-                    ->searchable(),
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 // TextColumn::make('business_type')
                 //     ->searchable(),
                 SelectColumn::make('company_size')
@@ -203,6 +267,7 @@ class ClientResource extends Resource
             'index' => Pages\ListClients::route('/'),
             'create' => Pages\CreateClient::route('/create'),
             'edit' => Pages\EditClient::route('/{record}/edit'),
+            'view' => Pages\ViewClient::route('/{record}/view'),
         ];
     }
 }
